51b87a83e58259eac69b3fb1a226dc20790a2fd6
aulin <hoque.talha@gmail.com>
2021-05-05 14:48:20 +0600

    index.html organized

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Shaan Mughal Food Service</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+
+    <!-- Font awesome -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Slick slider -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slick.css">
+    <!-- Date Picker -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-datepicker.css">
+    <!-- Fancybox slider -->
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.css" type="text/css" media="screen" />
+    <!-- Theme color -->
+    <link id="switcher" href="assets/css/theme-color/default-theme.css" rel="stylesheet">
+
+    <!-- Main style sheet -->
+    <link href="scss/style.css" rel="stylesheet">
+
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Prata' rel='stylesheet' type='text/css'>
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Pre Loader -->
+    <!-- <?php include "php/preloader.php" ?> -->
+
+
+    <!--START SCROLL TOP BUTTON -->
+
+    <?php include "php/scrolltop.php" ?>
+
+
+    <!-- Start header section -->
+    <?php include "php/header.php" ?>
+
+
+
+    <!-- Start slider  -->
+    <?php include "php/home/slider.php" ?>
+
+
+    <!-- Start About us -->
+    <?php include "php/home/about-us.php" ?>
+
+
+    <!-- Start Counter Section -->
+    <?php include "php/home/counter.php" ?>
+
+    <!-- Start Restaurant Menu -->
+    <?php include "php/home/menu.php" ?>
+
+
+
+
+    <!-- Start Gallery -->
+    <?php include "php/home/gallery.php" ?>
+
+
+    <!-- Start Client Testimonial section -->
+    <?php include "php/home/testimonial.php" ?>
+
+
+
+
+    <!-- Start Chef Section -->
+    <?php include "php/home/clients.php" ?>
+
+
+    <!-- Start Latest News -->
+    <?php include "php/home/latest-news.php" ?>
+
+
+    <!-- Start Contact section -->
+    <?php include "php/home/contact.php" ?>
+
+
+    <!-- Start Map section -->
+    <?php include "php/home/map.php" ?>
+
+
+    <!-- Start Footer -->
+    <?php include "php/footer.php" ?>
+
+
+    <!-- jQuery library -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- Slick slider -->
+    <script type="text/javascript" src="assets/js/slick.js"></script>
+    <!-- Counter -->
+    <script type="text/javascript" src="assets/js/waypoints.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.counterup.js"></script>
+    <!-- Date Picker -->
+    <script type="text/javascript" src="assets/js/bootstrap-datepicker.js"></script>
+    <!-- Mixit slider -->
+    <script type="text/javascript" src="assets/js/jquery.mixitup.js"></script>
+    <!-- Add fancyBox -->
+    <script type="text/javascript" src="assets/js/jquery.fancybox.pack.js"></script>
+
+    <!-- Custom js -->
+    <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
---
+++ php/preloader.php
@@ -0,0 +1,5 @@
+<div id="aa-preloader-area">
+        <div class="mu-preloader">
+            <img src="assets/img/preloader.gif" alt=" loader img">
+        </div>
+    </div>
