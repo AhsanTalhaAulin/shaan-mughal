501aea8d7565601b79e965ecf82e8f960d7c78d0
aulin <hoque.talha@gmail.com>
2021-05-07 14:31:43 +0600

    services section implemented

--- php/home/services.php
+++ php/home/services.php
@@ -11,107 +11,103 @@
                     </div>
 
                     <div class="content">
-                        <div class="tab-pane fade " id="services">
-                            <div class="sm-tab-content-area">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <div class="sm-tab-content-left">
-                                            <ul class="sm-menu-item-nav">
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">English Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$15.85</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">Chines Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$11.95</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">Indian Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$15.85</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            </ul>
+                        <div class="abc">
+                        <div class="sm-tab-content-area">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="sm-tab-content-left">
+                                                    <ul class="sm-menu-item-nav">
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Private Catering</a></h4>
+                                                                    
+                                                                    <p>Dawats, Birthdays, Anniversaries, Rooftop Parties, Outdoor Sports, Picnics, Bridal Showers, Baby Showers, Milad, Iftar and ShehriParties.</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Corporate Catering </a></h4>
+                                                                    
+                                                                    <p>Office Parties, Training Programs & Workshops, Meetings & Seminars, Sales Conferences, AGMs, Board Meetings, Town Hall Meetings, Company Celebrations, Customer Nights, Product Launch, Roadshows, University Club Events, University Competitions, University Events, Convocations etc</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Wedding- Engagement</a></h4>
+                                                                    <p>Akhth, Aiburo Bhat, Sangeet, Gaye Holud, Wedding, Reception, Post-Marriage Parties, Anniversary etc.</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="sm-tab-content-right">
+                                                    <ul class="sm-menu-item-nav">
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Tour Operator</a></h4>
+                                                                    <p>Coming Soon...</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Photography</a></h4>
+                                                                    <p>Coming Soon...</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="media">
+                                                                <div class="media-left">
+                                                                    <a href="#">
+                                                                        <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
+                                                                    </a>
+                                                                </div>
+                                                                <div class="media-body">
+                                                                    <h4 class="media-heading"><a href="#">Event Management</a></h4>
+                                                                    <p>Coming Soon...</p>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-6">
-                                        <div class="sm-tab-content-right">
-                                            <ul class="sm-menu-item-nav">
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">English Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$15.85</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-3.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">Chines Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$11.95</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                                <li>
-                                                    <div class="media">
-                                                        <div class="media-left">
-                                                            <a href="#">
-                                                                <img class="media-object" src="assets/img/menu/item-4.jpg" alt="img">
-                                                            </a>
-                                                        </div>
-                                                        <div class="media-body">
-                                                            <h4 class="media-heading"><a href="#">Indian Breakfast</a></h4>
-                                                            <span class="sm-menu-price">$15.85</span>
-                                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nulla aliquid praesentium dolorem commodi illo.</p>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
